b523f17697d915d3c2171ebe94fe165935925bea
AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
2023-12-11 14:52:05 +0100

    Added comments

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ function checkAnswers() {
             }
         } else {
             // Handle the case where no answer is selected for this question
-            // You can choose to deduct points, show a warning, or handle it as you see fit
             alert("Svara på alla frågor");
             return; // Stop checking further questions
         }
